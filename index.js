183ab8b5069802875917924fccbb7814877a1aca
aonikpaulalock <alockpaul9845@gmail.com>
2022-07-23 23:14:08 +0600

    aded

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
 // Middleware
 app.use(cors())
 app.use(express.json())
-require('dotenv').config()
 
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.w7k54.mongodb.net/?retryWrites=true&w=majority`;
