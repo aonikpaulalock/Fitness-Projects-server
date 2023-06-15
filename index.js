beb5938ac1e93a4183fcb0119ec90bb27b63ef7b
aonikpaulalock <alockpaul9845@gmail.com>
2023-06-15 21:39:50 +0600

    server changes to vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const ObjectId = require("mongodb").ObjectId;
 
 
